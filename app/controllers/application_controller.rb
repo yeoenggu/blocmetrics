dbfb68782d3641d6e5b506aaa0820c0d6a6ee70c
yeoenggu <yeoenggu@gmail.com>
2015-04-20 18:14:19 +0800

    Working client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  
   # protect_from_forgery with: :exception
+
 
   def self.cors_allowed_actions 
     @cors_allowed_actions ||= []
@@ -13,7 +15,7 @@ class ApplicationController < ActionController::Base
 
   def self.allow_cors(*methods)
     self.cors_allowed_actions += methods
-    before_filter :cors_allowed_actions, :only => methods
+    before_filter :cors_before_filter, :only => methods
     protect_from_forgery with: :null_session, :only => methods
   end
 
@@ -22,5 +24,11 @@ class ApplicationController < ActionController::Base
   # pass validation.
   def handle_unverified_request
     raise(ActionController::InvalidAuthenticityToken)
+  end
+
+  def cors_before_filter
+    if /\Ahttps?:\/\/localhost:3333\z/ =~ request.headers['Origin']
+      headers['Access-Control-Allow-Origin'] = request.headers['Origin']
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406064756) do
+ActiveRecord::Schema.define(version: 20150420075242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20150406064756) do
   create_table "events", force: :cascade do |t|
     t.string   "name"
     t.integer  "registered_application_id"
-    t.json     "payload",                   default: {}, null: false
+    t.json     "payload",                   default: {}
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
   end
