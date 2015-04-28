71755c5a8708e433e1c213f4a690c636dc19de91
yeoenggu <yeoenggu@gmail.com>
2015-04-28 18:21:57 +0800

    Added additional test case

--- spec/requests/create_event_spec.rb
+++ spec/requests/create_event_spec.rb
@@ -6,10 +6,10 @@ describe "Event Create API" do
     @registered_app = create :registered_application, url: Faker::Internet.domain_name
   end
 
-  it "create an event" do
+  it "create an event with payload" do
 
     parameters = {event: {name: "buy", payload: {product_name: "HeadFirstjQuery"}}}
-    # parameters_string = '{"event": {"name": "buy", "payload": {"product_name": "HeadFirstjQuery"}}}' 
+    
     headers = {Accept: "application/json", Origin: @registered_app.url , "Content-type": "application/json"}
     post "/api/events", parameters.to_json, headers
 
@@ -17,10 +17,27 @@ describe "Event Create API" do
     expect(response.content_type).to eq("application/json")
 
     expect(Event.count).to eq(1)
-    event = Event.first # refactor later ... lazy bum
+    event = Event.first 
 
-    expect(Event.first.name).to eq("buy")
-    expect(Event.first.payload).to eq({"product_name"=> "HeadFirstjQuery"})
-    expect(Event.first.registered_application).to eq @registered_app
+    expect(event.name).to eq("buy")
+    expect(event.payload).to eq({"product_name"=> "HeadFirstjQuery"})
+    expect(event.registered_application).to eq @registered_app
+  end
+
+  it "create an event without payload" do
+    parameters = {event: {name: "buy"}}
+
+    headers = {Accept: "application/json", Origin: @registered_app.url , "Content-type": "application/json"}
+    post "/api/events", parameters.to_json, headers
+
+    expect(response.status).to eq(201)
+    expect(response.content_type).to eq("application/json")
+
+    expect(Event.count).to eq(1)
+    event = Event.first 
+
+    expect(event.name).to eq("buy")
+    expect(event.payload).to eq(nil)
+    expect(event.registered_application).to eq @registered_app
   end
 end # end of "Event Create API"
